9e96ca5c4f020911c37e13e20adfcb7a4d21e0db
José Marcelino <jose.marcelino@jeknowledge.com>
2014-05-06 18:06:14 +0100

    add visual help

--- KingofKinect/KingofKinect/KingofKinect.cpp
+++ KingofKinect/KingofKinect/KingofKinect.cpp
@@ -8,20 +8,38 @@
 
 using namespace std;
 
+
 int main(){
 
 	NuiInitialize(NUI_INITIALIZE_FLAG_USES_SKELETON);
 	NUI_SKELETON_FRAME ourframe;
 
+	const int x = 10;
+	const int y = 10;
+
+	int tab[x][y];
+	for (int i = 0; i < x; i++){
+		for (int j = 0; j < y; j++){
+			tab[i][j] = 0;
+		}
+	}
+
+	
+	
 	while (1) //For all of time
 	{
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				cout << tab[i][j];
+			}
+			cout << endl;
+		}
+
 		NuiSkeletonGetNextFrame(0, &ourframe); //Get a frame and stuff it into ourframe
 		for (int i = 0; i < 6; i++) //Six times, because the Kinect has space to track six people
 		{
 			if (ourframe.SkeletonData[i].eTrackingState == NUI_SKELETON_TRACKED) //See more on this line below
-				cout << "Right Hand: "; //Print "Right hand:"
-			cout << ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HAND_RIGHT].x << endl; //See more on this line below
-			cout << ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HAND_RIGHT].y << endl;
+				tab[(int)((ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HAND_RIGHT].y * 10))][(int)((ourframe.SkeletonData[i].SkeletonPositions[NUI_SKELETON_POSITION_HAND_RIGHT].x * 10))] = 1;
 		}
 		system("cls");//Clear the screen
 	}
